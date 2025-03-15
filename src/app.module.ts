3e89aa5f79dc729b0bc667f42ec0188897e57c55
zzyoon <yun.zz.del@gmail.com>
2025-03-15 14:48:47 +0900

    🎉 Nestjs folder setting
    nestjs folder setting

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { DatebaseModule } from './datebase/datebase.module';
+import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(), DatebaseModule, ProductModule],
   controllers: [AppController],
   providers: [AppService],
 })
